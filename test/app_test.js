28d789a41bd8b60f509e28bab18129e12ae3e786
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-07 18:26:48 -0500

    moved connect function() to app.js

--- test/app_test.js
+++ test/app_test.js
@@ -1,10 +1,5 @@
 const assert = require('assert');
 const request = require('supertest');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/muber')
-
 const app = require('../app');
 
 describe("The Express app", () => {
