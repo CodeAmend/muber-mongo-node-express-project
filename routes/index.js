c3586fc33f6d2935baa719b5e8355869e9d3ce0c
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-07 18:41:28 -0500

    Added a PUT request for updating

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const DriverControllers = require('../controllers/drivers_controller');
 module.exports = (app) => {
 
   app.get('/api', DriverControllers.greeting);
-
   app.post('/api/drivers', DriverControllers.create);
+  app.put('/api/drivers/:id', DriverControllers.edit);
 
 }
