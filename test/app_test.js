b59af016cfeffb5d3ef906d0885ab07fb8817d88
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-07 11:40:38 -0500

    Added mongoose.connect() for mongodb

--- test/app_test.js
+++ test/app_test.js
@@ -1,5 +1,10 @@
 const assert = require('assert');
 const request = require('supertest');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber')
+
 const app = require('../app');
 
 describe("The Express app", () => {
