29514a100eb1eaf6eba89ce73cb92d571f747c28
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-07 18:27:23 -0500

    Added custom error handling middleware

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
 const routes = require('./routes');
 
+mongoose.Promise = global.Promise;
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber');
+}
+
 app.use(bodyParser.json())
 
 routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
 
 module.exports = app;
