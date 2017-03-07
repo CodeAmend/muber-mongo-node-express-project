76bc504a74ce4f2c4474dfba88fbe2ac8495bbe6
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-07 11:35:27 -0500

    Added require statement so Diver will be instantiated with mocha

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,8 +1,10 @@
+const Driver = require('../models/driver');
+
 module.exports = {
   greeting(err, response) {
     response.send({ hi: "there" });
   },
   create(err, response) {
-    
+    response.send({ bye: "there"});
   }
 }
